e9bb63f8348dc727212007d0428bc1443c297ca8
Ayaan <ayaan2911@aol.com>
2023-05-11 18:43:51 +0530

    Initial Commit

---
+++ Sonority_iOS/Sonority_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sonority_iOSApp.swift
+//  Sonority_iOS
+//
+//  Created by Ayaan on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Sonority_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
